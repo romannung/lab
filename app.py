84f1aec4d4384eea13bffab1e6e7e5f8ea2b178b
Roman <roman.ivanyshyn-ip213@nung.edu.ua>
2025-05-26 13:31:06 +0300

    integrated sentry for analytics

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import logging
+import sentry_sdk
 
 logging.basicConfig(
     filename='app.log',
@@ -6,4 +7,12 @@ logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s'
 )
 
+sentry_sdk.init(
+   dsn="https://d84816fa1e98d1d28a75d82abba00db4@o4509388742131712.ingest.de.sentry.io/4509388747243600",
+   send_default_pii=True,
+   traces_sample_rate=1.0
+)
+
 logging.info("Додаток запущено")
+raise Exception("Тестова помилка для Sentry")
+
